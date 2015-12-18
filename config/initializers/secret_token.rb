25852b4c43f20a34e406806b944a436a70cab394
Brady Fukumoto <bradyfukumoto@gmail.com>
2015-12-18 10:16:42 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdaptiveLearning::Application.config.secret_key_base = 'd90e37d3da0337b735b14e183b1dd6a7f69b7711fc394d8d09ab428a1811cc28fd5086ebb909924d02b7ff5d400e98098392351cbcacec62c255f74a0276f838'
