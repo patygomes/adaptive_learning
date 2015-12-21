786184a16021da62f28c225e0e9c6f3fbdfef563
Brady Fukumoto <bradyfukumoto@gmail.com>
2015-12-21 15:40:36 -0800

    Adding title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,23 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @title = "Home"
   end
+
+  def definition
+    @title = "Definition"
+  end
+
+  def quiz
+    @title = "Quiz"
+  end
+
+  def product
+    @title = "Product"
+  end
+
+  def about
+    @title = "About"
+  end
+
 end
