cfaad4f9e9f0bc804c06f40b2f1877d32b32850a
Brady Fukumoto <bradyfukumoto@gmail.com>
2015-12-21 16:30:20 -0800

    Added stylesheets

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,23 +1,18 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @title = "Home"
   end
 
   def definition
-    @title = "Definition"
   end
 
   def quiz
-    @title = "Quiz"
   end
 
   def product
-    @title = "Product"
   end
 
   def about
-    @title = "About"
   end
 
 end
